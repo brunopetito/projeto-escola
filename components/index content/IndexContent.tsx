806d7381b972d3252f26a4297c3fdd3790b6584a
Bruno Petito <brunopetito97@gmail.com>
2022-12-17 19:33:25 -0300

    adicionando carousel

---
+++ components/index content/IndexContent.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Slider from '../Slider';
+
+export default function IndexContent() {
+  return (
+    <div className=" w-full flex flex-col  h-screen ">
+      <div className="bg-red-100">
+        <Slider />
+      </div>
+    </div>
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,15 @@
 import Footer from '../components/Footer';
 import Header from '../components/Header';
+import IndexContent from '../components/index content/IndexContent';
 import Layout from '../components/Layout';
 
 export default function Home() {
   return (
     <div className="text-3xl">
       <Layout>
-        <div className="w-full">
+        <div className="w-full flex flex-col  ">
           <Header></Header>
+          <IndexContent />
         </div>
         <Footer></Footer>
       </Layout>
