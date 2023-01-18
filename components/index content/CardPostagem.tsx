4ab4b1695e46ee7e910a1996d2303ce8beb450cd
Bruno Petito <brunopetito97@gmail.com>
2023-01-18 12:22:10 -0300

    reparando bug do card das postagens

--- components/index content/CardPostagem.tsx
+++ components/index content/CardPostagem.tsx
@@ -16,8 +16,8 @@ export default function Postagem({
   imgFixed
 }: PostagemProps) {
   return (
-    <div className="max-w-xs bg-white border border-gray-200 rounded-lg shadow-md  ">
-      <a href={linkTo ? linkTo : '#'}>
+    <a href={linkTo ? linkTo : '#'}>
+      <div className="max-w-xs bg-white border border-gray-200 rounded-lg shadow-md  ">
         <picture>
           <img
             className="rounded-t-lg"
@@ -25,15 +25,15 @@ export default function Postagem({
             alt=""
           />
         </picture>
-      </a>
-      <div className="p-5">
-        <a href={linkTo ? linkTo : '#'}>
+
+        <div className="p-5">
           <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 ">
             {titulo}
           </h5>
-        </a>
-        <p className="mb-3 font-normal text-gray-700 ">{subtitulo}</p>
+
+          <p className="mb-3 font-normal text-gray-700 ">{subtitulo}</p>
+        </div>
       </div>
-    </div>
+    </a>
   );
 }
